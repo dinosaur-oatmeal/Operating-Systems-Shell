fbcaeb40d8dbcf79fb86a860e3263d5f94120223
dinosaur-oatmeal <139949595+dinosaur-oatmeal@users.noreply.github.com>
2024-02-08 13:45:53 -0600

    Add files via upload
    updated msh.c to incorporate exit and fork. Looking for help with exec

--- msh/msh.c
+++ msh/msh.c
@@ -1,3 +1,5 @@
+// Name: Will Maberry
+//
 // The MIT License (MIT)
 // 
 // Copyright (c) 2024 Trevor Bakker 
@@ -29,10 +31,128 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
+#include <ctype.h>
+#include <dirent.h>
 
+#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
+                                // so we need to define what delimits our tokens.
+                                // In this case  white space
+                                // will separate the tokens on our command line
 
-int main( int argc, char * argv[] )
+#define MAX_COMMAND_SIZE 255    // The maximum command-line size
+
+#define MAX_NUM_ARGUMENTS 32
+
+int main()
 {
-  return 0;
+
+  char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
+
+  while( 1 )
+  {
+    // Print out the msh prompt
+    printf ("msh> ");
+
+    // Read the command from the commandi line.  The
+    // maximum command that will be read is MAX_COMMAND_SIZE
+    // This while command will wait here until the user
+    // inputs something.
+    while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
+
+    /* Parse input */
+    char *token[MAX_NUM_ARGUMENTS];
+
+    int token_count = 0;                                 
+                                                           
+    // Pointer to point to the token
+    // parsed by strsep
+    char *argument_pointer;                                         
+                                                           
+    char *working_string  = strdup( command_string );                
+
+    // we are going to move the working_string pointer so
+    // keep track of its original value so we can deallocate
+    // the correct amount at the end
+    
+    char *head_ptr = working_string;
+    
+    // Tokenize the input with whitespace used as the delimiter
+    while ( ( (argument_pointer = strsep(&working_string, WHITESPACE ) ) != NULL) &&
+              (token_count<MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup( argument_pointer, MAX_COMMAND_SIZE );
+      if( strlen( token[token_count] ) == 0 )
+      {
+        token[token_count] = NULL;
+      }
+        token_count++;
+    }
+
+    // Now print the tokenized input as a debug check
+    // \TODO Remove this code and replace with your shell functionality
+
+    //compare first token to exit
+    if(strcmp(token[0], "exit") == 0)
+    {
+      //printf("Success!");
+
+      //exit program
+      exit(1);
+    }
+
+    // all processes that require fork()
+    else
+    {
+      // make child's pid and status
+      pid_t child_pid = fork();
+      int status;
+        
+      // *** commands that I'll likely need (from notes) ***
+      // execl(char const *path, char const *arg0, ...);
+      // execle(char const *path, char const *arg0, ..., char const *envp[]);
+      // execlp(char const *file, char const *arg0, ...);
+      // execv(char const *path, char const *argv[]);
+      // execve(char const *path, char const *argv[], char const *envp[]);
+      // execvp(char const *file, char const *argv[]);
+
+      // child pid error
+      if( child_pid == -1)
+      {
+        perror("fork fialed: ");
+        exit(EXIT_FAILURE);
+      }
+
+      // execute child pid's process
+      if(child_pid == 0)
+      {
+          // needs to be changed to reflect token[]
+          execl("/bin/ls", "ls", NULL);
+
+          fflush(NULL);
+          exit(EXIT_SUCCESS);
+      }
+
+      // wait for child process to finish
+      waitpid(child_pid, &status, 0);
+
+      // childe terminated by signal
+      if(WIFSIGNALED(status))
+      {
+        printf("Child returned with status: %d", WTERMSIG(status));
+      }
+    }
+
+    // remove next 5 lines at the end
+    int token_index  = 0;
+    for( token_index = 0; token_index < token_count; token_index ++ ) 
+    {
+      printf("token[%d] = %s\n", token_index, token[token_index] );  
+    }
+
+    free( head_ptr );
+
+  }
+  return 0; 
 }
 
